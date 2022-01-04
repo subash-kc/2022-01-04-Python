4e4653a7ae5b0fc3cace6c84d518806442a8831a
KC <mksubash@amazon.com>
2022-01-04 14:17:29 -0800

    Committing Object and Methods

---
+++ learningListOFList.optional.challenge.py
@@ -0,0 +1,7 @@
+animals_list = ["Fox", "Fly", "Ant", "Bee", "Cod", "Cat", "Dog", "Yak", "Cow", "Hen", "Koi", "Hog", "Jay", "Kit"]
+
+print(animals_list)
+
+animals_list.extend("Subash")
+
+print(animals_list)
