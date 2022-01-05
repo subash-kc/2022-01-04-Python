898b1d6fae15e42a1fad965cdd103ab896260262
KC <mksubash@amazon.com>
2022-01-05 13:38:47 -0800

    If Else Challenge Exercise

---
+++ challenge.ifelse.py
@@ -0,0 +1,47 @@
+
+score = float(input("Enter the Score: \n"))
+
+
+if score>=90 and score<=100:
+    print("Your final grade is A")
+
+elif score>=80 and score<90:
+    print("Your final Grade is B")
+
+elif score>=70 and score<80:
+    print("Your final Grade is C")
+
+elif score>=60 and score<70:
+    print("Your final Grade is D")
+
+elif score<60 and score>=0:
+    print("Your final Grade is F")
+
+else:
+    print("Not a valid input")
+
+
+## Slaffir-Simpson WindSpeed
+
+"""
+If/Else challenge exercise
+School Person Grade
+"""
+windspeed = int(input("Enter the Windspeeds in Km/hr: \n"))
+
+def speed(windspeed):
+    if windspeed>= 252:
+        return "Five"
+    elif windspeed>=209 and windspeed < 252:
+        return "Four"
+    elif windspeed>=178 and windspeed < 208:
+        return "Three"
+    elif windspeed>=154 and windspeed < 177:
+        return "Two"
+    elif windspeed>=119 and windspeed< 154:
+        return "One"
+    else:
+        print("Invalid Input")
+
+print("WindSpeed Category is: ", speed(windspeed))
+
--- lab24.py
+++ lab24.py
@@ -20,13 +20,19 @@ if ipchk2:
 else:    # if data is NOT provided
    print("You did not provide input.") # indented under else
 
+def main():
 
-ipchk3 = input("Apply an IP address3: ") # this line now prompts the user for input
+   ipchk3 = input("Apply an IP address3: ") # this line now prompts the user for input
 
 # if user set IP of gateway
-if ipchk3 == "192.168.70.1":
-   print("Looks like the IP address3 of the Gateway was set: " + ipchk3 + " This is not recommended.")
-elif ipchk3: # if any data is provided, this will test true
-   print("Looks like the IP address3 was set: " + ipchk3) # indented under if
-else: # if data is NOT provided
-   print("You did not provide input.") # indented under else
+   if ipchk3 == "192.168.70.1":
+      print("Looks like the IP address3 of the Gateway was set: " + ipchk3 + " This is not recommended.")
+   elif ipchk3: # if any data is provided, this will test true
+      print("Looks like the IP address3 was set: " + ipchk3) # indented under if
+   else: # if data is NOT provided
+      print("You did not provide input.") # indented under else
+
+if __name__ == '__main__':
+    main()
+
+print("Subash")
