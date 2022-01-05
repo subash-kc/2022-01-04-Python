c0e3e7d50120f296c5b1cd7aaa40d473c135f45f
KC <mksubash@amazon.com>
2022-01-05 07:37:24 -0800

    Working with Input and Slicing

---
+++ arithmeticops.py
@@ -0,0 +1,10 @@
+
+a = 125;
+
+b = 5;
+
+print(a//b)
+
+print(b**4)
+
+print(b//3) 
---
+++ learnstringslicing.py
@@ -0,0 +1,30 @@
+#slicing = create a substring by extracting elements from another string
+            # indexing[] or slice()
+            #[start:stop:step]
+
+name = "Subash KC"
+
+first_name = name[0:6]
+
+print(first_name)
+
+last_name = name[7:9]
+
+print(last_name)
+
+print(first_name + " " + last_name)
+
+#to find the length of the string
+print(len(name))
+
+funky_name = "ArvindaSwamy Lamsal"
+
+step_example = funky_name[::3]
+
+reversed_name = funky_name[::-1]
+print(reversed_name)
+
+print(step_example)
+
+
+
