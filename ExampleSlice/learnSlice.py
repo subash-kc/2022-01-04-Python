a74e6d27769a1cc02b28c5090a197ba0633c966b
KC <mksubash@amazon.com>
2022-01-06 06:50:32 -0800

    Some of the Example worked in slicing and Ditionaries

---
+++ ExampleSlice/learnSlice.py
@@ -0,0 +1,16 @@
+website1 = "http://localhost:3000"
+
+slice_local = slice(7, -5)
+
+print(website1[slice_local])
+
+website2 = "http://google.com"
+
+slice_google = slice(7, -4)
+
+print(website2[slice_google])
+
+website3 = "http://wikipedia.com"
+
+slice_wikipedia = slice(7, -4)
+print(website3[slice_wikipedia])
