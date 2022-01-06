04b24cb25647db00dffbeb12922f5882aad4c9c6
KC <mksubash@amazon.com>
2022-01-06 07:02:21 -0800

    Basic of the index operator

---
+++ ExampleSlice/indexoperator.py
@@ -0,0 +1,20 @@
+# index operator[] = gives access to a sequence's element (str, list, tuples)
+
+
+name = "subash kC!!!"
+
+
+#if(name[0].islower()):
+    #name = name.capitalize()
+
+first_name = name[0:6].upper()
+
+last_name = name[7:].upper()
+
+print(first_name)
+
+print(last_name)
+
+last_character = name[-2]
+
+print(last_character)
