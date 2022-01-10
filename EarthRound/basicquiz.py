5eff201dd242c36ae964ac1bb1b251e8c1b089a1
KC <mksubash@amazon.com>
2022-01-10 07:27:39 -0800

    Add Clear function to Word Game

--- EarthRound/basicquiz.py
+++ EarthRound/basicquiz.py
@@ -4,31 +4,38 @@ print("-----------------------------------------------------------")
 Defining all the necessary to function
 new game function to start the game
 """
-def new_game():
-    #guesses is empty list in the beginning
-    guesses = []
-    #correct guesses set to 0 . we haven't guesssed anything
-    correct_guesses = 0
-    question_number = 1 # represent the first question
 
-    #We need to display all the questions in our dictionary of questions
+
+def clear_screen(lines):
+    for l in range(lines):
+        print()
+
+def new_game():
+    # guesses is empty list in the beginning
+    guesses = []
+    # correct guesses set to 0 . we haven't guesssed anything
+    correct_guesses = 0
+    question_number = 1  # represent the first question
+
+    # We need to display all the questions in our dictionary of questions
+    clear_screen(20)
     for key in questions:
+        clear_screen(35)
         print()
         print(key)
 
-        #nested for loop to display all the options. Indexing starts from 0. Use some sorts of counter
+        # nested for loop to display all the options. Indexing starts from 0. Use some sorts of counter
         for i in options[question_number - 1]:
             print(i)
 
+        # User input to prompt the choices
+        guess = input("Enter (A, B, C, or D): ").upper()  # String case sensitive and give them a point
+        guesses.append(guess)  # emplty list og guesses and apend our guess
 
-        #User input to prompt the choices
-        guess = input("Enter (A, B, C, or D): ").upper() #String case sensitive and give them a point
-        guesses.append(guess) #emplty list og guesses and apend our guess
-
-        #calling check answer function to check answer by passing few answers argument.
-        #incrementing correct guesses by one
+        # calling check answer function to check answer by passing few answers argument.
+        # incrementing correct guesses by one
         correct_guesses += check_answer(questions.get(key), guess)
-        #incrementing by 1
+        # incrementing by 1
         question_number += 1
 
     display_score(correct_guesses, guesses)
@@ -39,15 +46,18 @@ def new_game():
 Function to check the answer
 
 """
+
+
+# parameter of answer and guess
 def check_answer(answer, guess):
-    #checking our answer to the correct answer
+    # checking our answer to the correct answer
     if answer == guess:
-        print("CORRECT")
+        print("Your answer is CORRECT!!!!")
         # return 1 point if we scored a point
         return 1
     else:
-        print("WRONG")
-        #donot get a point if we donot score a point
+        print("You gave a WRONG answer!!!")
+        # donot get a point if we donot score a point
         return 0
 
 
@@ -56,6 +66,8 @@ def check_answer(answer, guess):
 """
 Function to Display Score
 """
+
+
 def display_score(correct_guesses, guesses):
     print("-------------------------------")
     # print("RESULTS")
@@ -82,15 +94,15 @@ def display_score(correct_guesses, guesses):
 """
 Function to ask user to play again
 """
+
+
 def play_again():
     response = input("Do you want to play again? (yes or no): ").upper()
 
-    if response =="YES":
+    if response == "YES":
         return True
     else:
         return False
-
-
 
 
 # -----------------------------------------------------------
@@ -111,12 +123,13 @@ questions = {
 2-D List to hold all the possible answers to each of these questions. List of list. Each list corresponds to the dictonary
 of the questions
 """
-options = [["A. Leopard", "B. Cheetah", "C. Tiger", "D. Kangaroo"], # all the options for 1st question
-           ["A. Charles Babbage", "B. Bob Kahn", "C. Alan Turing", "D. Tim Cranner"], # all the options for 2nd question
-           ["A. Mount K2", "B. Mount Denali", "C. C. Mount Makalu", "D. Mount Everest"], # all the options for 3rd
-           ["A. 10", "B. 11", "C. 12", "D. 9"], #all the options for 4th question
-           ["A. George Washington", "B. John Adams", "C. Thomas Jefferson", "D. James Madison"]] #all the options for 5th question
-
+options = [["A. Leopard", "B. Cheetah", "C. Tiger", "D. Kangaroo"],  # all the options for 1st question
+           ["A. Charles Babbage", "B. Bob Kahn", "C. Alan Turing", "D. Tim Cranner"],
+           # all the options for 2nd question
+           ["A. Mount K2", "B. Mount Denali", "C. C. Mount Makalu", "D. Mount Everest"],  # all the options for 3rd
+           ["A. 10", "B. 11", "C. 12", "D. 9"],  # all the options for 4th question
+           ["A. George Washington", "B. John Adams", "C. Thomas Jefferson",
+            "D. James Madison"]]  # all the options for 5th question
 
 new_game()
 
